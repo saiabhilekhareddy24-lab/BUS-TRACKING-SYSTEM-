e5a681359881c85acebcf936d7e7ca205602c055
saiabhilekhareddy24-lab <saiabhilekhareddy24@gmail.com>
2026-08-20 14:14:00 +0530

    Create testbench_output.py

---
+++ testbench_output.py
@@ -0,0 +1,14 @@
+test_bus_id (test_bus_tracking.TestBusTracking) ... ok
+test_initial_position (test_bus_tracking.TestBusTracking) ... ok
+test_invalid_progress (test_bus_tracking.TestBusTracking) ... ok
+test_position_at_end (test_bus_tracking.TestBusTracking) ... ok
+test_position_at_middle (test_bus_tracking.TestBusTracking) ... ok
+test_position_at_start (test_bus_tracking.TestBusTracking) ... ok
+test_speed (test_bus_tracking.TestBusTracking) ... ok
+test_start_bus (test_bus_tracking.TestBusTracking) ... ok
+test_stop_bus (test_bus_tracking.TestBusTracking) ... ok
+
+----------------------------------------------------------------------
+Ran 9 tests in 0.001s
+
+OK
